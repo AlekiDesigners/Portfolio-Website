41360cfbba3c7d7748e083724cd57cb936d099e5
Alex Designer <wmraz@students.prairiestate.edu>
2024-02-28 00:25:31 +0000

    added html landing page

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-  return render_template('home.html')
-
+def home():
+  return render_template("home.html")
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host= "0.0.0.0", port=5000)
